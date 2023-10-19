496d9a1219a967fe8ba5503811b77abe434a3139
muntinovaa <tolganay.muntinova@gmail.com>
2023-10-19 00:14:37 -0500

    test

---
+++ src/main/java/org/example/StringAlgorithm/MergeTwoStringInterleave.java
@@ -0,0 +1,41 @@
+package org.example.StringAlgorithm;
+
+/**
+ Problem Statement: Merge two strings by interleaving their characters.
+ Input:  String 1: "abc", String 2: "def"
+ Output: Merged String: "adbecf"
+ Description:
+ Given two input strings,
+ the goal is to merge them such that the characters from each string are interleaved.
+ For the provided input strings "abc" and "def", the output should be "adbecf".
+ **/
+public class MergeTwoStringInterleave {
+    public static void main(String[] args) {
+        String s1 = "abc";
+        String s2 = "def";
+
+        System.out.println(mergeStrings(s1, s2));  // Output: adbecf
+    }
+
+    public static String mergeStrings(String s1, String s2) {
+        StringBuilder result = new StringBuilder();
+        int i = 0, j = 0;
+
+        while (i < s1.length() && j < s2.length()) {
+            result.append(s1.charAt(i++));
+            result.append(s2.charAt(j++));
+        }
+        // Append any remaining characters from either string
+        while (i < s1.length()) {
+            result.append(s1.charAt(i++));
+        }
+        while (j < s2.length()) {
+            result.append(s2.charAt(j++));
+        }
+
+        return result.toString();
+    }
+}
+
+
+
---
+++ src/main/java/org/example/StringAlgorithm/StringReplaceUsingRegularExpressions.java
@@ -0,0 +1,24 @@
+package org.example.StringAlgorithm;
+
+/**
+ Using Java 8, write a program that replaces all occurrences of the word "sams" with "walmart" in a given string.
+ The replacement should be case-insensitive.
+ Given the string:
+ S = "sams abbdd SAM dd sam.. jj ..sam be"
+ The output should be:
+ "walmart abbdd walmart dd walmart.. jj ..walmart be"
+ (?i): This flag ensures the regex matching is case-insensitive.
+ sam: The substring we're looking to replace.
+ */
+public class StringReplaceUsingRegularExpressions {
+
+
+
+        public static void main(String[] args) {
+            String s = "sams abbdd SAM dd sam.. jj ..sam be";
+
+            String replacedString = s.replaceAll("(?i)sam", "walmart");
+
+            System.out.println(replacedString);
+        }
+}
