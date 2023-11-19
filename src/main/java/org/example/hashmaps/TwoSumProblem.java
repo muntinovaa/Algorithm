eadade7094f4db3614c9464cc98a617e7102ba8b
muntinovaa <tolganay.muntinova@gmail.com>
2023-11-19 14:13:57 -0600

    test

--- src/main/java/org/example/hashmaps/TwoSumProblem.java
+++ src/main/java/org/example/hashmaps/TwoSumProblem.java
@@ -1,4 +1,8 @@
 package org.example.hashmaps;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
 
@@ -21,10 +25,26 @@ package org.example.hashmaps;
 
  * */
 public class TwoSumProblem {
-    /*
-    public int[] twoSum(int[] nums, int target) {
-
+    public static void main(String[] args) {
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = twoSum(nums, target);
+        System.out.println("Indices: [" + result[0] + ", " + result[1] + "]");
     }
-    */
+
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (numMap.containsKey(complement)) {
+                return new int[] {numMap.get(complement), i};
+            }
+            numMap.put(nums[i], i);
+        }
+
+        throw new IllegalArgumentException("No two sum solution");
+    }
+
 
 }
