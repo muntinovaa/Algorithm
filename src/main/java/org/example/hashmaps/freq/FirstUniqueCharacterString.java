fc1147dddce2d37c5eed009194af1d5884b0732e
muntinovaa <tolganay.muntinova@gmail.com>
2023-11-19 22:34:38 -0600

    test

--- src/main/java/org/example/hashmaps/freq/FirstUniqueCharacterString.java
+++ src/main/java/org/example/hashmaps/freq/FirstUniqueCharacterString.java
@@ -1,20 +1,19 @@
 package org.example.hashmaps.freq;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class FirstUniqueCharacterString {
     public static void main(String[] args) {
         String str = "leetcode";
-        int index = firstUniqueChar(str);
-        if (index != -1) {
-            System.out.println("First unique character is at index: " + index);
-            System.out.println(str.charAt(index));
-        } else {
-            System.out.println("No unique character found.");
-        }
+        Character char1 = firstUniqueChar(str);
+        Character char2 = firstUniqueChar1(str);
+        System.out.println(char1);
+        System.out.println(char2);
+
     }
 
-    public static Integer firstUniqueChar(String str){
+    public static Character firstUniqueChar(String str){
         HashMap<Character,Integer> charCount = new HashMap<>();
 
         for (int i=0; i<str.length(); i++){
@@ -24,10 +23,26 @@ public class FirstUniqueCharacterString {
 
         for (int i=0; i<str.length(); i++){
             if (charCount.get(str.charAt(i)) == 1) {
-                return i;
+                return str.charAt(i);
             }
         }
 
-      return -1;
+      return null;
+    }
+
+    public static Character firstUniqueChar1(String str) {
+        Map<Character, Integer> charCount = new HashMap<>();
+
+        for (char c : str.toCharArray()) {
+            charCount.put(c, charCount.getOrDefault(c, 0) + 1);
+        }
+
+        for (char c : str.toCharArray()) {
+            if (charCount.get(c) == 1) {
+                return c;
+            }
+        }
+
+        return null; // Return null if no unique character is found
     }
 }
