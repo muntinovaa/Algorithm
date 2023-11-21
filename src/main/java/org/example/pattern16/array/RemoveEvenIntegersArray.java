c80ab283133aca170daf8b1f3a14cdf4ea3271c2
muntinovaa <tolganay.muntinova@gmail.com>
2023-11-21 12:09:32 -0600

    test

--- src/main/java/org/example/pattern16/array/RemoveEvenIntegersArray.java
+++ src/main/java/org/example/pattern16/array/RemoveEvenIntegersArray.java
@@ -6,15 +6,14 @@ public class RemoveEvenIntegersArray {
     public static void main(String[] args) {
 
         int[] arr1 = {1, 1, 2, 2, 3, 4, 4, 5, 5};
-        for (int j : arr1) {
-            System.out.print(j + " , "); // print array
-        }
         System.out.println();
-        int[] newArr1 =  removeEven(arr1); // calling removeEven
-        for (int j : newArr1) {
-            System.out.print(j + "  , "); // print array
-        }
-
+        int[] newArr1 =  removeEven(arr1);  // calling removeEven
+        int[] newArr2 =  removeEven1(arr1); // calling removeEven
+        int[] newArr3 =  removeEven2(arr1); // calling removeEven
+        System.out.println(Arrays.toString(arr1));
+        System.out.println(Arrays.toString(newArr1));
+        System.out.println(Arrays.toString(newArr2));
+        System.out.println(Arrays.toString(newArr3));
     }
     public static int[] removeEven(int[] arr) {
         int oddElements = 0;
@@ -30,4 +29,16 @@ public class RemoveEvenIntegersArray {
         return result;
     }
 
+    public static int[] removeEven1(int[] arr) {
+        return Arrays.stream(arr).filter(num -> num % 2 != 0).toArray();
+    }
+    public static int[] removeEven2(int[] arr) {
+        int j = 0;
+        for (int num : arr) {
+            if (num % 2 != 0) {
+                arr[j++] = num;
+            }
+        }
+        return Arrays.copyOf(arr, j);
+    }
 }
