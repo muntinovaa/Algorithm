19e2f116f9034695cfdb834e84baa56ec5f1195c
muntinovaa <tolganay.muntinova@gmail.com>
2023-11-21 12:00:06 -0600

    test

--- src/main/java/org/example/pattern16/array/RemoveEvenIntegersArray.java
+++ src/main/java/org/example/pattern16/array/RemoveEvenIntegersArray.java
@@ -1,32 +1,31 @@
 package org.example.pattern16.array;
 
+import java.util.Arrays;
+
 public class RemoveEvenIntegersArray {
     public static void main(String[] args) {
-        int size = 10;
-        int[] arr = new int[size]; //declaration and instantiation
-        System.out.print("Before removing Even Numbers: ");
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = i; // assigning values
-            System.out.print(arr[i] +  " ");
+
+        int[] arr1 = {1, 1, 2, 2, 3, 4, 4, 5, 5};
+        for (int j : arr1) {
+            System.out.print(j + " , "); // print array
         }
         System.out.println();
-
-        int[] newArr =  removeEven(arr); // calling removeEven
-        System.out.print("After removing Even Numbers: ");
-        for (int j : newArr) {
-            System.out.print(j + " "); // print array
+        int[] newArr1 =  removeEven(arr1); // calling removeEven
+        for (int j : newArr1) {
+            System.out.print(j + "  , "); // print array
         }
+
     }
     public static int[] removeEven(int[] arr) {
         int oddElements = 0;
-        for (int j : arr) {
-            if (j % 2 != 0) oddElements++;
+        for (int num : arr) {
+            if (num % 2 != 0) oddElements++;
         }
         int[] result = new int[oddElements];
         int result_index = 0;
-        for (int j : arr) {
-            if (j % 2 != 0)
-                result[result_index++] = j;
+        for (int num : arr) {
+            if (num % 2 != 0)
+                result[result_index++] = num;
         }
         return result;
     }
