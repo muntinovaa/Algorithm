6c6e1df28a290876200faa5c9674285f2a9de362
muntinovaa <tolganay.muntinova@gmail.com>
2023-11-21 22:26:45 -0600

    test

--- src/main/java/org/example/pattern16/arraylist/RemoveDuplicatesArrayList.java
+++ src/main/java/org/example/pattern16/arraylist/RemoveDuplicatesArrayList.java
@@ -1,29 +1,28 @@
 package org.example.pattern16.arraylist;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class RemoveDuplicatesArrayList {
     public static void main(String[] args) {
-        ArrayList<String> list = new ArrayList<>();
-        list.add("apple");
-        list.add("banana");
-        list.add("apple");
-        list.add("orange");
-        list.add("banana");
-
-        ArrayList<String> noDupes1 = removeDuplicates1(list);
-
-        System.out.println(noDupes1);  // Outputs: [apple, banana, orange]
-        list.add("banana");
-        list.add("banana");
-        list.add("bananagsdgsdgsgsdgsdgsdg");
-        System.out.println(list);  // Outputs: [apple, banana, orange]
-        ArrayList<String> noDupes2 = removeDuplicates2(list);
-        System.out.println(noDupes2);  // Outputs: [apple, banana, orange]
+        ArrayList<String> inputList = new ArrayList<>(Arrays.asList("apple", "banana", "apple", "cherry", "banana"));
+        System.out.println(inputList);
+        System.out.println(removeDuplicates(inputList));
     }
+    public static ArrayList<String> removeDuplicates(ArrayList<String> list) {
+        ArrayList<String> uniqueList = new ArrayList<>();
 
+        for (String item : list) {
+            if (!uniqueList.contains(item)) {
+                uniqueList.add(item);
+            }
+        }
+
+        return uniqueList; // Return the list with duplicates removed
+    }
     public static ArrayList<String> removeDuplicates1(ArrayList<String> list) {
       HashSet<String> set = new HashSet<>(list);
       return new ArrayList<>(set);
@@ -35,4 +34,7 @@ public class RemoveDuplicatesArrayList {
                    .collect(Collectors.toCollection(ArrayList::new));
     }
     //https://github.com/NirmalSilwal/Data-Structure-and-Algorithm-Java-interview-kit
+
+
+
 }
